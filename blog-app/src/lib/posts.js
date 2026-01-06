772906338549502b13da92be346040b8827336b3
HomamZituni <homam93@msn.com>
2026-01-05 22:19:01 -0500

    added fake blog post data and auth context and provider

---
+++ blog-app/src/lib/posts.js
@@ -0,0 +1,20 @@
+export const posts = [
+  {
+    id: 1,
+    slug: "my-first-post",
+    title: "My First Blog Post",
+    content: "This is the content of my first blog post. Welcome to my blog!"
+  },
+  {
+    id: 2,
+    slug: "learning-react-router",
+    title: "Learning React Router",
+    content: "React Router makes navigation in React apps incredibly easy and powerful."
+  },
+  {
+    id: 3,
+    slug: "dynamic-routing-explained",
+    title: "Dynamic Routing Explained",
+    content: "Dynamic routing allows us to create pages based on URL parameters."
+  }
+];

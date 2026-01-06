2054ba2a510de47a31d6185b52ab7d64aa5e217a
HomamZituni <homam93@msn.com>
2026-01-05 23:59:25 -0500

    added routing and browser routing to App.jsx so app will load

---
+++ blog-app/src/pages/Admin.jsx
@@ -0,0 +1,8 @@
+export function Admin() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to the Admin Dashboard</h1>
+      <p>This is a protected page.</p>
+    </div>
+  );
+}
